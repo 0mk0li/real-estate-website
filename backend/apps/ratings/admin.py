3832a8e262ccc37e500672a3099ed56c62cdf51c
0mk0li <om.official.17@gmail.com>
2025-06-08 16:32:30 +0530

    rating models and serializers.

--- backend/apps/ratings/admin.py
+++ backend/apps/ratings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Ratings
 
 # Register your models here.
+
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ["rater", "agent", "ratings"]
+
+
+admin.site.register(Ratings, RatingAdmin)
